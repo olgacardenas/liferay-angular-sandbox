41427055cc0653ebe984d3cc6b4f50aeca069217
Olga Helena <olgasan@Olgas-MacBook-Pro.local>
2015-08-26 11:25:12 -0500

    refactor to include jshint gulp task for checking portlets source code

--- misc/copa/gulpfile.js
+++ misc/copa/gulpfile.js
@@ -10,60 +10,14 @@ var jshint = require('gulp-jshint');
 
 /**
  * File patterns
+ * ----------------------------------------------------
  **/
-
-// Root directory
 var rootDirectory = path.resolve('./');
-
-// Source directory for build process
 var sourceDirectory = path.join(rootDirectory, './src');
 
-/*
- * Section for global functionality
+/**
+ * Run tests once and exit
  * ----------------------------------------------------
- */
-
-var sourceFiles = [
-
-  // Make sure module files are handled first
-  path.join(sourceDirectory, '/**/*.module.js'),
-
-  // Ignore independent portlet files
-  path.join('!' + sourceDirectory, '/**/*-portlet.js'),
-
-  // Then add all JavaScript files
-  path.join(sourceDirectory, '/**/*.js')
-];
-
-var lintFiles = [
-  'gulpfile.js',
-  // Karma configuration
-  'karma-*.conf.js'
-].concat(sourceFiles);
-
-gulp.task('build', function() {
-  gulp.src(sourceFiles)
-    .pipe(plumber())
-    .pipe(concat('copa.js'))
-    .pipe(gulp.dest('./dist/'))
-    .pipe(uglify())
-    .pipe(rename('copa.min.js'))
-    .pipe(gulp.dest('./dist'));
-});
-
-/**
- * Validate source JavaScript
- */
-gulp.task('jshint', function () {
-  return gulp.src(lintFiles)
-    .pipe(plumber())
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(jshint.reporter('fail'));
-});
-
-/**
- * Run test once and exit
  */
 gulp.task('test-src', function (done) {
   karma.start({
@@ -72,9 +26,6 @@ gulp.task('test-src', function (done) {
   }, done);
 });
 
-/**
- * Run test once and exit
- */
 gulp.task('test-dist-concatenated', function (done) {
   karma.start({
     configFile: __dirname + '/karma-dist-concatenated.conf.js',
@@ -82,9 +33,6 @@ gulp.task('test-dist-concatenated', function (done) {
   }, done);
 });
 
-/**
- * Run test once and exit
- */
 gulp.task('test-dist-minified', function (done) {
   karma.start({
     configFile: __dirname + '/karma-dist-minified.conf.js',
@@ -92,49 +40,74 @@ gulp.task('test-dist-minified', function (done) {
   }, done);
 });
 
-/**
- * Copy distribution files to liferay theme
+/*
+ * Functionality inserted inside liferay theme
+ * ----------------------------------------------------
  */
-gulp.task('copy-to-theme', function() {
-  gulp.src('./dist/copa.js')
-    .pipe(gulp.dest('../../themes/copa-theme/src/main/webapp/js'));
-  gulp.src('./dist/copa.min.js')
+
+var themeSourceFiles = [
+  path.join(sourceDirectory, '/**/*.module.js'), // Make sure module files are handled first
+  path.join('!' + sourceDirectory, '/**/*-portlet.js'), // Ignore independent portlet files
+  path.join(sourceDirectory, '/**/*.js') // Then add all JavaScript files
+];
+
+gulp.task('build', function() {
+  gulp.src(themeSourceFiles)
+    .pipe(plumber())
+    .pipe(concat('copa.js'))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('../../themes/copa-theme/src/main/webapp/js'))
+    .pipe(uglify())
+    .pipe(rename('copa.min.js'))
+    .pipe(gulp.dest('./dist'))
     .pipe(gulp.dest('../../themes/copa-theme/src/main/webapp/js'));
 });
 
 /**
- * Section for individual portlet js
+ * Functionality inserted inside individual portlets
  * ----------------------------------------------------
  */
 
 //Proof of concept: B portlet
+var bPortletName = 'b-portlet';
+var bPortletSourceFiles = path.join(sourceDirectory, '/**/' + bPortletName + '.js');
 gulp.task('build-b-portlet', function() {
-  gulp.src(path.join(sourceDirectory, '/**/b-portlet.js'))
+  gulp.src(bPortletSourceFiles)
     .pipe(plumber())
-    .pipe(concat('b-portlet.js'))
+    .pipe(concat(bPortletName + '.js'))
     .pipe(gulp.dest('./dist/'))
     .pipe(uglify())
-    .pipe(rename('b-portlet.min.js'))
-    .pipe(gulp.dest('./dist'));
-});
-gulp.task('copy-to-b-portlet', function() {
-  gulp.src('./dist/b-portlet.min.js')
+    .pipe(rename(bPortletName + '.min.js'))
+    .pipe(gulp.dest('./dist'))
     .pipe(gulp.dest('../../portlets/test-portlets/src/main/webapp/js'));
 });
 
 /**
- * All
+ * Execute for all
  * ----------------------------------------------------
  */
 
-gulp.task('copy-to-liferay', function (done) {
-  runSequence('copy-to-theme', 'copy-to-b-portlet', done);
+// Validate source JavaScript
+var lintFiles = [
+  'gulpfile.js',
+  'karma-*.conf.js',
+  bPortletSourceFiles
+].concat(themeSourceFiles);
+
+gulp.task('jshint', function () {
+  return gulp.src(lintFiles)
+    .pipe(plumber())
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
 });
 
+// Group tasks
 gulp.task('process-all', function (done) {
-  runSequence('jshint', 'test-src', 'build', 'build-b-portlet', 'copy-to-liferay', done);
+  runSequence('jshint', 'test-src', 'build', 'build-b-portlet', done);
 });
 
+// Make default
 gulp.task('default', function () {
   runSequence('process-all');
 });
